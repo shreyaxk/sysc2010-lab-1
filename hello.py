2e0edf5fe1ca9195fb5f9982111b01619b05682d
shreyaxk <shreyakaushal002@gmail.com>
2026-01-09 17:16:40 -0500

    Update hello.py
    updated w imports

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("hello sysc2010")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+print("Libraries imported successfully!")
+#Create a small series 
+data = pd.Series([1,3,5,np.nan,6,8])
+print(data)
