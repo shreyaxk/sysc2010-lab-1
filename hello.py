f64e49ec00c482ec31558bb8c7f54430739c95ec
shreyaxk <shreyakaushal002@gmail.com>
2026-01-20 10:33:03 -0500

    Delete hello.py

--- hello.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-print("Libraries imported successfully!")
-#Create a small series 
-data = pd.Series([1,3,5,np.nan,6,8])
-print(data)
-print("Hello world")
